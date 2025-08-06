8ae1c1cccaae5bf1d45375efc255cdc41602db93
rito528 <39003544+rito528@users.noreply.github.com>
2025-08-06 18:28:21 +0900

    feat: アクセスログが表示されるようにする

--- src/main/scala/com/example/todoapp/Main.scala
+++ src/main/scala/com/example/todoapp/Main.scala
@@ -3,16 +3,25 @@ package com.example.todoapp
 import cats.effect.{ IO, IOApp, ExitCode }
 import com.comcast.ip4s._
 import org.http4s.ember.server._
+import org.http4s.client.middleware.Logger
+import org.http4s.client.Client
 
 object Main extends IOApp {
 
   def run(args: List[String]): IO[ExitCode] = {
+    val client = Client.fromHttpApp(TodoappRoutes.routes.orNotFound)
+
+    val loggerClient = Logger[IO](
+      logHeaders = true,
+      logBody    = true,
+      logAction  = Some((msg: String) => IO.println(msg))
+    )(client)
 
     EmberServerBuilder
       .default[IO]
       .withHost(ipv4"0.0.0.0")
       .withPort(port"9000")
-      .withHttpApp(TodoappRoutes.pingRoutes)
+      .withHttpApp(loggerClient.toHttpApp)
       .build
       .use(_ => IO.never)
       .as(ExitCode.Success)
--- src/main/scala/com/example/todoapp/TodoappRoutes.scala
+++ src/main/scala/com/example/todoapp/TodoappRoutes.scala
@@ -1,18 +1,16 @@
 package com.example.todoapp
 
 import org.http4s.dsl.io.*;
-import org.http4s.implicits.*;
 import cats.effect.IO
 import org.http4s.HttpRoutes
-import cats.data.Kleisli
 import org.http4s.Response
 import org.http4s.Request
 
 object TodoappRoutes {
-  def pingRoutes: Kleisli[IO, Request[cats.effect.IO], Response[cats.effect.IO]] = {
+  def routes: HttpRoutes[IO] = {
     HttpRoutes.of[IO] {
       case GET -> Root / "ping" =>
         Ok("ok")
-    }.orNotFound
+    }
   }
 }
