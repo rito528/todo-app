33d6ad8d0ad7aa229c227f76bd9d00a25d09646e
rito528 <39003544+rito528@users.noreply.github.com>
2025-08-06 17:31:50 +0900

    feat: ping を返すエンドポイントの実装

--- src/main/scala/com/example/todoapp/Main.scala
+++ src/main/scala/com/example/todoapp/Main.scala
@@ -1,6 +1,21 @@
 package com.example.todoapp
 
-import cats.effect.{IO, IOApp}
+import cats.effect.{ IO, IOApp, ExitCode }
+import com.comcast.ip4s._
+import org.http4s.ember.server._
 
-object Main extends IOApp.Simple:
-  val run = TodoappServer.run[IO]
+object Main extends IOApp {
+
+  def run(args: List[String]): IO[ExitCode] = {
+
+    EmberServerBuilder
+      .default[IO]
+      .withHost(ipv4"0.0.0.0")
+      .withPort(port"9000")
+      .withHttpApp(TodoappRoutes.pingRoutes)
+      .build
+      .use(_ => IO.never)
+      .as(ExitCode.Success)
+  }
+
+}
--- src/main/scala/com/example/todoapp/TodoappRoutes.scala
+++ src/main/scala/com/example/todoapp/TodoappRoutes.scala
@@ -1,30 +1,18 @@
 package com.example.todoapp
 
-import cats.effect.Sync
-import cats.syntax.all.*
+import org.http4s.dsl.io.*;
+import org.http4s.implicits.*;
+import cats.effect.IO
 import org.http4s.HttpRoutes
-import org.http4s.dsl.Http4sDsl
+import cats.data.Kleisli
+import org.http4s.Response
+import org.http4s.Request
 
-object TodoappRoutes:
-
-  def jokeRoutes[F[_]: Sync](J: Jokes[F]): HttpRoutes[F] =
-    val dsl = new Http4sDsl[F]{}
-    import dsl.*
-    HttpRoutes.of[F] {
-      case GET -> Root / "joke" =>
-        for {
-          joke <- J.get
-          resp <- Ok(joke)
-        } yield resp
-    }
-
-  def helloWorldRoutes[F[_]: Sync](H: HelloWorld[F]): HttpRoutes[F] =
-    val dsl = new Http4sDsl[F]{}
-    import dsl.*
-    HttpRoutes.of[F] {
-      case GET -> Root / "hello" / name =>
-        for {
-          greeting <- H.hello(HelloWorld.Name(name))
-          resp <- Ok(greeting)
-        } yield resp
-    }
+object TodoappRoutes {
+  def pingRoutes: Kleisli[IO, Request[cats.effect.IO], Response[cats.effect.IO]] = {
+    HttpRoutes.of[IO] {
+      case GET -> Root / "ping" =>
+        Ok("ok")
+    }.orNotFound
+  }
+}
