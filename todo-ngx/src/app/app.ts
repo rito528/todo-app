3aa06512df03dffe99fe1d83d3c2bd4342e26e43
rito528 <39003544+rito528@users.noreply.github.com>
2025-08-13 16:40:40 +0900

    feat: TODO の追加をできるようにする

--- todo-ngx/src/app/app.ts
+++ todo-ngx/src/app/app.ts
@@ -4,10 +4,22 @@ import { HttpClient } from '@angular/common/http';
 import { MatTableModule } from '@angular/material/table';
 import { Todo } from '../types';
 import { TodoStatePipe } from './pipes/todo-state-pipe';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatTableModule, TodoStatePipe],
+  imports: [
+    RouterOutlet,
+    MatTableModule,
+    TodoStatePipe,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    ReactiveFormsModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.sass'
 })
@@ -26,4 +38,18 @@ export class App {
 
   displayedColumns: string[] = ["title", "body", "state", "category"]
 
+  createTodoForm = new FormGroup({
+    title: new FormControl(''),
+    body: new FormControl('')
+  })
+
+  onSubmitCreateTodo() {
+    this.http.post<Todo>('/api/todos', {
+      title: this.createTodoForm.controls.title.value,
+      body: this.createTodoForm.controls.body.value
+    }).subscribe({
+      next: (todo) => this.todos = [...this.todos, todo],
+      error: (err) => console.error(err)
+    })
+  }
 }
