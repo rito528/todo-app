9dd351d55cfab84649324c300ef95bc058675301
rito528 <39003544+rito528@users.noreply.github.com>
2025-08-15 10:50:16 +0900

    feat: Category 用リポジトリの定義・実装をする

--- lib-domain/src/main/scala/com/example/domain/CategoryRepository.scala
+++ lib-domain/src/main/scala/com/example/domain/CategoryRepository.scala
@@ -7,4 +7,23 @@ trait CategoryRepository[F[_]] {
     */
   def fetchAllCategory: F[Vector[Category]]
 
+  /**
+    * @param category 作成したい [[Category]]
+    * @return [[Category]] 作成し、作成した [[Category]] の [[CategoryId]] を返す作用
+    */
+  def createCategory(category: Category): F[CategoryId]
+
+  /**
+    * @param categoryId 更新したい [[Category]] の [[CategoryId]]
+    * @param category 更新後の [[Category]]
+    * @return `categoryId` に紐づく [[Category]] を `category` に更新する作用
+    */
+  def updateCategory(categoryId: CategoryId, category: Category): F[Unit]
+
+  /**
+    * @param categoryId 削除したい [[Category]] に紐づく [[CategoryId]]
+    * @return `categoryId` に紐づく [[Category]] を削除する作用
+    */
+  def deleteCategory(categoryId: CategoryId): F[Unit]
+
 }
--- lib-infrastructure-mysql/src/main/scala/com/example/infra/CategoryRepositoryImpl.scala
+++ lib-infrastructure-mysql/src/main/scala/com/example/infra/CategoryRepositoryImpl.scala
@@ -9,6 +9,7 @@ import com.example.domain.CategorySlug
 import com.example.domain.CategoryName
 import com.example.domain.CategoryId
 import com.example.domain.CategoryColor
+import cats.implicits.*
 
 class CategoryRepositoryImpl[F[_]: Async](
   using pool: DatabaseConnectionPool
@@ -17,13 +18,12 @@ class CategoryRepositoryImpl[F[_]: Async](
   override def fetchAllCategory: F[Vector[Category]] = pool.transactor.use { xa =>
     given categoryRead: Read[Category] = Read[(Int, String, String, String)]
       .map { case (id, name, slug, color) =>
-
-        Category( 
+        Category(
           Some(CategoryId(id)),
           CategoryName(name),
           CategorySlug(slug),
           CategoryColor(color)
-        )  
+        )
       };
 
     sql"SELECT id, name, slug, color FROM category"
@@ -31,4 +31,46 @@ class CategoryRepositoryImpl[F[_]: Async](
       .to[Vector]
       .transact(xa)
   }
+
+  override def createCategory(category: Category): F[CategoryId] = pool.transactor.use { xa =>
+    (for {
+      _  <- sql"""
+      | INSERT INTO category (name, slug, color) 
+      | VALUES (${category.name.unwrap}, ${category.slug.unwrap}, ${category.color.unwrap})"""
+        .stripMargin
+        .update
+        .run
+      id <- sql"SELECT LAST_INSERT_ID()".query[Int].unique
+    } yield CategoryId(id))
+      .transact(xa)
+  }
+
+  override def updateCategory(categoryId: CategoryId, category: Category): F[Unit] = pool.transactor.use { xa =>
+    sql"""
+    | UPDATE category SET
+    | name = ${category.name.unwrap}
+    | slug = ${category.slug.unwrap}
+    | color = ${category.color.unwrap}
+    | WHERE id = ${categoryId.unwrap}"""
+      .stripMargin
+      .update
+      .run
+      .transact(xa)
+      .void
+  }
+
+  override def deleteCategory(categoryId: CategoryId): F[Unit] = pool.transactor.use { xa =>
+    (for {
+      _ <- sql"DELETE FROM category WHERE id = ${categoryId.unwrap}"
+        .update
+        .run
+      _ <- sql"UPDATE to_do SET category_id = NULL WHERE category_id = ${categoryId.unwrap}"
+        .update
+        .run
+    } yield ())
+      .transact(xa)
+      .void
+
+  }
+
 }
