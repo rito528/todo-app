23774df019a4571ee1991309b275272001209a48
rito528 <39003544+rito528@users.noreply.github.com>
2025-08-15 14:36:51 +0900

    refactor: Todo 作成コンポーネントの分割

--- todo-ngx/src/app/app.ts
+++ todo-ngx/src/app/app.ts
@@ -1,32 +1,17 @@
 import { Component, inject, Injectable } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
-import { MatTableModule } from '@angular/material/table';
 import { Category, Todo } from '../types';
-import { TodoStatePipe } from './pipes/todo-state-pipe';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSelectModule } from '@angular/material/select';
-import { MatIconModule } from '@angular/material/icon';
-import { MatMenuModule } from '@angular/material/menu';
 import { MatDialog } from '@angular/material/dialog';
 import { TodoTable } from './components/todo-table/todo-table';
+import { CreateTodoForm } from './components/create-todo-form/create-todo-form';
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    MatTableModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatButtonModule,
-    ReactiveFormsModule,
-    MatSelectModule,
-    MatIconModule,
-    MatMenuModule,
-    TodoTable
+    TodoTable,
+    CreateTodoForm
   ],
   templateUrl: './app.html',
   styleUrl: './app.sass'
@@ -47,30 +32,6 @@ export class App {
 
     this.http.get<Category[]>('/api/categories').subscribe({
       next: (categories) => this.categories = categories,
-      error: (err) => console.error(err)
-    })
-  }
-
-  createTodoForm = new FormGroup({
-    title: new FormControl(''),
-    body: new FormControl(''),
-    category: new FormControl<undefined | number>(undefined)
-  })
-
-  onSubmitCreateTodo() {
-    this.http.post<Todo>('/api/todos', {
-      title: this.createTodoForm.controls.title.value,
-      body: this.createTodoForm.controls.body.value,
-      categoryId: this.createTodoForm.controls.category.value
-    }).subscribe({
-      next: (todo) => this.todos = [...this.todos, todo],
-      error: (err) => console.error(err)
-    })
-  }
-
-  onDeleteTodo(todoId: number) {
-    this.http.delete(`/api/todos/${todoId}`).subscribe({
-      next: () => this.todos = this.todos.filter((todo) => todo.id !== todoId),
       error: (err) => console.error(err)
     })
   }
